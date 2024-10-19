80e3015deb2365daf8ede2772c8f56348a9b81b9
Dermot Duffy <dermot.duffy@gmail.com>
2024-10-19 16:00:03 -0700

    Split into proxy and library

--- custom_components/hass_proxy/const.py
+++ custom_components/hass_proxy/const.py
@@ -1,9 +1,26 @@
 """Constants for hass_proxy."""
 
 from logging import Logger, getLogger
+from typing import Final
 
-DOMAIN = "hass_proxy"
+from homeassistant.util.ssl import SSLCipherList
 
-CONF_URLS = "urls"
+DOMAIN: Final = "hass_proxy"
 
 LOGGER: Logger = getLogger(__package__)
+
+CONF_SSL_VERIFICATION: Final = "ssl_verification"
+CONF_SSL_CIPHERS: Final = "ssl_ciphers"
+CONF_SSL_CIPHER_INSECURE: Final = SSLCipherList.INSECURE
+CONF_SSL_CIPHER_MODERN: Final = SSLCipherList.MODERN
+CONF_SSL_CIPHER_INTERMEDIATE: Final = SSLCipherList.INTERMEDIATE
+CONF_SSL_CIPHER_PYTHON_DEFAULT: Final = SSLCipherList.PYTHON_DEFAULT
+
+CONF_DYNAMIC_URLS: Final = "dynamic_urls"
+CONF_URL_PATTERNS: Final = "url_patterns"
+
+DEFAULT_OPTIONS: dict[str, str | bool | list[str]] = {
+    CONF_SSL_VERIFICATION: True,
+    CONF_DYNAMIC_URLS: True,
+    CONF_SSL_CIPHERS: CONF_SSL_CIPHER_PYTHON_DEFAULT,
+}
