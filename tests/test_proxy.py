47c787e91faf8fee1a9e32c42fd283a263949c98
Dermot Duffy <dermot.duffy@gmail.com>
2025-08-28 20:58:05 -0700

    feat: Add support for websocket proxying (#103)

--- tests/test_proxy.py
+++ tests/test_proxy.py
@@ -2,12 +2,15 @@
 
 from __future__ import annotations
 
+import asyncio
 import datetime
-import urllib
+import urllib.parse
 import uuid
 from http import HTTPStatus
+from types import MappingProxyType
 from typing import TYPE_CHECKING, Any
 
+import aiohttp
 import pytest
 from homeassistant.exceptions import ServiceValidationError
 
@@ -40,12 +43,14 @@ from tests import (
 if TYPE_CHECKING:
     from homeassistant.core import HomeAssistant
 
-TEST_OPTIONS = {
-    CONF_DYNAMIC_URLS: True,
-    CONF_SSL_CIPHERS: "default",
-    CONF_SSL_VERIFICATION: True,
-    CONF_URL_PATTERNS: [],
-}
+TEST_OPTIONS = MappingProxyType(
+    {
+        CONF_DYNAMIC_URLS: True,
+        CONF_SSL_CIPHERS: "default",
+        CONF_SSL_VERIFICATION: True,
+        CONF_URL_PATTERNS: [],
+    }
+)
 
 TEST_SERVICE_CALL_PARAMS = {
     CONF_OPEN_LIMIT: 0,
@@ -63,10 +68,12 @@ async def test_proxy_view_success(
     """Test that a valid URL proxies successfully."""
     config_entry = create_mock_hass_web_proxy_config_entry(
         hass,
-        {
-            **TEST_OPTIONS,
-            CONF_URL_PATTERNS: [str(local_server)],
-        },
+        MappingProxyType(
+            {
+                **TEST_OPTIONS,
+                CONF_URL_PATTERNS: [str(local_server)],
+            }
+        ),
     )
     await setup_mock_hass_web_proxy_config_entry(hass, config_entry)
     await async_proxy_setup_entry(hass, config_entry)
@@ -102,10 +109,12 @@ async def test_proxy_view_no_matching_url(
     """Verify proxying without a matching URL."""
     config_entry = create_mock_hass_web_proxy_config_entry(
         hass,
-        {
-            **TEST_OPTIONS,
-            CONF_URL_PATTERNS: [],
-        },
+        MappingProxyType(
+            {
+                **TEST_OPTIONS,
+                CONF_URL_PATTERNS: [],
+            }
+        ),
     )
     await setup_mock_hass_web_proxy_config_entry(hass, config_entry)
     await async_proxy_setup_entry(hass, config_entry)
@@ -135,12 +144,14 @@ async def test_proxy_view_ssl_cipher_no_verify(
     """Verify proxying with insecure SSL settings."""
     config_entry = create_mock_hass_web_proxy_config_entry(
         hass,
-        {
-            **TEST_OPTIONS,
-            CONF_URL_PATTERNS: [str(local_server)],
-            CONF_SSL_CIPHERS: ssl_cipher,
-            CONF_SSL_VERIFICATION: False,
-        },
+        MappingProxyType(
+            {
+                **TEST_OPTIONS,
+                CONF_URL_PATTERNS: [str(local_server)],
+                CONF_SSL_CIPHERS: ssl_cipher,
+                CONF_SSL_VERIFICATION: False,
+            }
+        ),
     )
     await setup_mock_hass_web_proxy_config_entry(hass, config_entry)
     await async_proxy_setup_entry(hass, config_entry)
@@ -202,10 +213,11 @@ async def test_proxy_view_dynamic_url_return_value(
         return_response=True,
     )
 
+    assert service_response
     assert isinstance(service_response.get("url_id"), str)
 
     # Ensure the response is a valid UUID (will raise if not).
-    uuid.UUID(service_response["url_id"], version=4)
+    uuid.UUID(str(service_response["url_id"]), version=4)
 
 
 async def test_proxy_view_dynamic_url_delete(
@@ -393,3 +405,45 @@ async def test_proxy_view_dynamic_url_unauthenticated_permitted(
         f"/api/hass_web_proxy/v0/?url={urllib.parse.quote_plus(str(local_server))}"
     )
     assert resp.status == HTTPStatus.OK
+
+
+async def test_proxy_websocket_view_success(
+    hass: HomeAssistant,
+    local_server: Any,
+    hass_client: Any,
+) -> None:
+    """Test that a valid URL proxies successfully."""
+    config_entry = create_mock_hass_web_proxy_config_entry(hass, TEST_OPTIONS)
+
+    await setup_mock_hass_web_proxy_config_entry(hass, config_entry)
+    await async_proxy_setup_entry(hass, config_entry)
+
+    url_to_proxy = str(local_server) + "ws"
+
+    await hass.services.async_call(
+        DOMAIN,
+        SERVICE_CREATE_PROXIED_URL,
+        {
+            **TEST_SERVICE_CALL_PARAMS,
+            CONF_URL_PATTERN: url_to_proxy,
+        },
+        blocking=True,
+    )
+
+    authenticated_hass_client = await hass_client()
+
+    async with authenticated_hass_client.ws_connect(
+        f"/api/hass_web_proxy/v0/ws?url={urllib.parse.quote_plus(url_to_proxy)}"
+    ) as ws:
+        await ws.ping()
+
+        request = await ws.receive_json()
+        assert request["url"] == f"{local_server}ws"
+
+        # Test sending text data.
+        result = await asyncio.gather(
+            ws.send_str("hello!"),
+            ws.receive(),
+        )
+        assert result[1].type == aiohttp.WSMsgType.TEXT
+        assert result[1].data == "hello!"
